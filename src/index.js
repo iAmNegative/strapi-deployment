50213ded8389584d1fd4cba7454c177fe1fa01d2
prathamesh satpute <negativesatpute@gmail.com>
2023-09-09 13:41:19 +0530

    socket installed in strapi

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-'use strict';
+"use strict";
+const { default: axios } = require("axios");
 
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+  register({ strapi }) {},
+  
+  bootstrap({ strapi }) {
+    var io = require("socket.io")(strapi.server.httpServer, {
+      cors: {
+        // origin: "http://localhost:3000",
+        origin: "https://react-demo-7a31.onrender.com",
+
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
+      },
+    });
+
+    io.on("connection", function (socket) {
+      socket.on("join", ({ username }) => {
+        console.log("user connected");
+        console.log("username is ", username);
+        if (username) {
+          socket.join("group");
+          socket.emit("welcome", {
+            user: "bot",
+            text: `${username}, Welcome to the group chat`,
+            userData: username,
+          });
+        } else {
+          console.log("An error occurred");
+        }
+      });
+
+      socket.on("sendMessage", async (data) => {
+
+        io.emit("message", {
+          receiverUser: data.receiverUser
+        }, (error) => {// Sending the message to the backend
+          if (error) {
+          }
+        });
+
+      });
+    });
+  },
 };
