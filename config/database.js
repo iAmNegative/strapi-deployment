166c36ed92ee41001441045a88e039bf074bab5b
prathamesh satpute <prathameshsatpute20@gmail.com>
2024-12-13 19:33:56 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 module.exports = ({ env }) => ({
 
   connection: {
@@ -10,7 +10,11 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL'),
+        // ssl: env.bool('DATABASE_SSL'),
+        ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(env('DATABASE_SSLROOTCERT_PATH')).toString()
+    },
       },
     },
   });
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ module.exports = {
   bootstrap({ strapi }) {
     var io = require("socket.io")(strapi.server.httpServer, {
       cors: {
-        // origin: "http://localhost:3000",
-        origin: "https://react-demo-7a31.onrender.com",
+        origin: "http://localhost:3000",
+        // origin: "https://react-demo-7a31.onrender.com",
 
 
         methods: ["GET", "POST"],
@@ -47,3 +47,31 @@ module.exports = {
     });
   },
 };
+
+const https = require('https');
+
+exports.handler = async (event, context) => {
+ const url = 'https://yoursitehere.onrender.com';
+
+ return new Promise((resolve, reject) => {
+   const req = https.get(url, (res) => {
+     if (res.statusCode === 200) {
+       resolve({
+         statusCode: 200,
+         body: 'Server pinged successfully',
+       });
+     } else {
+       reject(
+         new Error(`Server ping failed with status code: ${res.statusCode}`)
+       );
+     }
+   });
+
+   req.on('error', (error) => {
+     reject(error);
+   });
+
+   req.end();
+ });
+};
+
